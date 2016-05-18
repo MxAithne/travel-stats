05361030062d62dff86986e167d5bf1dfe231075
Aithne <MxAithne@gmail.com>
2016-05-18 18:33:50 +0100

    Better version of heat cal, start of week monday now

--- drawgraphs.js
+++ drawgraphs.js
@@ -124,80 +124,79 @@ function drawBarChart()
 }
 
 /* Calendar Heat Graph */
+
 var cellSize = 17; // cell size
+
+var day = function(d) { return (d.getDay() + 6) % 7; }, // monday = 0
+    week = d3.time.format("%W"), // monday-based week number
+    date = d3.time.format("%Y-%m-%d"),
+    percent = d3.format("+.1%");
+
+var margin = {top: 5.5, right: 0, bottom: 5.5, left: 19.5},
+    width = 960 - margin.left - margin.right,
+    height = 130 - margin.top - margin.bottom,
+    size = height / 7;
+
+var color = d3_scale.scaleMagma()
+    .domain([0, 5])
 
 function drawHeatCal()
 {
-  console.log("Drawing heat calendar");
-
-  var width = 960,
-      height = 136;
-
-  var percent = d3.format(".1%"),
-      format = d3.time.format("%Y-%m-%d");
-
-  var color = d3.scale.quantize()
-      .domain([0, 5])
-      .range(d3.range(11).map(function(d) { return "q" + d + "-11"; }));
-
-  var svg = d3.select("#calheat")
-      .selectAll("svg")
+  
+  var svg = d3.select("#calheat").selectAll("svg")
       .data(d3.range(2016, 2017))
-      .enter().append("svg")
-      .attr("width", width)
-      .attr("height", height)
+    .enter().append("svg")
       .attr("class", "RdYlGn")
-      .append("g")
-      .attr("transform", "translate(" + ((width - cellSize * 53) / 2) + "," + (height - cellSize * 7 - 1) + ")");
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   svg.append("text")
-      .attr("transform", "translate(-6," + cellSize * 3.5 + ")rotate(-90)")
-      .style("text-anchor", "middle")
+      .attr("transform", "translate(-6," + size * 3.5 + ")rotate(-90)")
+      .attr("text-anchor", "middle")
       .text(function(d) { return d; });
 
   var rect = svg.selectAll(".day")
       .data(function(d) { return d3.time.days(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
-      .enter().append("rect")
+    .enter().append("rect")
       .attr("class", "day")
-      .attr("width", cellSize)
-      .attr("height", cellSize)
-      .attr("x", function(d) { return d3.time.weekOfYear(d) * cellSize; })
-      .attr("y", function(d) { return d.getDay() * cellSize; })
-      .datum(format);
+      .attr("width", size)
+      .attr("height", size)
+      .attr("x", function(d) { return week(d) * size; })
+      .attr("y", function(d) { return day(d) * size; })
+      .datum(date);
 
   rect.append("title")
       .text(function(d) { return d; });
 
   svg.selectAll(".month")
       .data(function(d) { return d3.time.months(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
-      .enter().append("path")
+    .enter().append("path")
       .attr("class", "month")
       .attr("d", monthPath);
 
-  d3.csv("travel-data.csv", function(error, csv) {
-    if (error) throw error;
-
+  d3.csv("travel-data.csv", function(csv) {
     var data = d3.nest()
       .key(function(d) { return d.Date; })
       .rollup(function(d) { return d.length; })
       .map(csv);
 
     rect.filter(function(d) { return d in data; })
-      .attr("class", function(d) { return "day " + color(data[d]); })
+      .attr("style", function(d) { return "fill:" + color(data[d]); })
+      //.attr("class", function(d) { return "day q" + color(data[d]) + "-9"; })
       .select("title")
       .text(function(d) { return d + ": " + data[d]; });
   });
 }
 
-function monthPath(t0)
-{
+function monthPath(t0) {
   var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
-      d0 = t0.getDay(), w0 = d3.time.weekOfYear(t0),
-      d1 = t1.getDay(), w1 = d3.time.weekOfYear(t1);
-  return "M" + (w0 + 1) * cellSize + "," + d0 * cellSize
-      + "H" + w0 * cellSize + "V" + 7 * cellSize
-      + "H" + w1 * cellSize + "V" + (d1 + 1) * cellSize
-      + "H" + (w1 + 1) * cellSize + "V" + 0
-      + "H" + (w0 + 1) * cellSize + "Z";
+      d0 = +day(t0), w0 = +week(t0),
+      d1 = +day(t1), w1 = +week(t1);
+  return "M" + (w0 + 1) * size + "," + d0 * size
+      + "H" + w0 * size + "V" + 7 * size
+      + "H" + w1 * size + "V" + (d1 + 1) * size
+      + "H" + (w1 + 1) * size + "V" + 0
+      + "H" + (w0 + 1) * size + "Z";
 }
-
